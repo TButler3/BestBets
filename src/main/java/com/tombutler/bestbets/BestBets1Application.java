8e8476ebfd8c7a1fcc6caa604d14536c57c0483f
Tom Butler <tfbt5987@gmail.com>
2022-09-29 11:19:05 -0400

    test

---
+++ src/main/java/com/tombutler/bestbets/BestBets1Application.java
@@ -0,0 +1,13 @@
+package com.tombutler.bestbets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestBets1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestBets1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tombutler/bestbets/BestBets1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tombutler.bestbets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestBets1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
