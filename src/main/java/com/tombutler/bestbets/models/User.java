4ad01bb20b48ab14782fcb8e60cf702e21ca6abd
Tom Butler <tfbt5987@gmail.com>
2022-09-30 01:43:57 -0400

    user model

--- src/main/java/com/tombutler/bestbets/models/User.java
+++ src/main/java/com/tombutler/bestbets/models/User.java
@@ -1,6 +1,21 @@
 package com.tombutler.bestbets.models;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotEmpty(message="Username is required!")
+	@Size(min=3, max=28, message="Username must be between 3 and 28 characters")
 	private String username;
+	
 }
