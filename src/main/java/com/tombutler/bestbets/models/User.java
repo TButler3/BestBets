c309cea89aa7c229e1c820963ab8788416f3e1b8
Tom Butler <tfbt5987@gmail.com>
2022-09-30 13:36:04 -0400

    user model completed

--- src/main/java/com/tombutler/bestbets/models/User.java
+++ src/main/java/com/tombutler/bestbets/models/User.java
@@ -1,10 +1,21 @@
 package com.tombutler.bestbets.models;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name="users")
@@ -17,5 +28,83 @@ public class User {
 	@NotEmpty(message="Username is required!")
 	@Size(min=3, max=28, message="Username must be between 3 and 28 characters")
 	private String username;
+	
+	@NotEmpty(message="Email is required!")
+	@Email(message="Not a valid email")
+	private String email;
+	
+	@NotEmpty(message="Password required!")
+	@Size(min=8, max=48, message="Password must be between 8 and 48 characters")
+	private String password;
+	
+	@Transient
+	@NotEmpty(message="Please confirm password")
+	private String confirmPassword;
+	
+	@Column(updatable=false)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date createdAt;
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date updatedAt;
+	
+	public User() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	@PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+	
 	
 }
